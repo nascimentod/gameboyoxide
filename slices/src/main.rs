77f1b39bdba9104028573f05e1f993acdd645189
David Nascimento <david.nascimento@kebrasoft.com>
2024-05-04 09:28:01 +0200

    chapter 4 from the rust book

---
+++ slices/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let s = "hello world";
+
+    let first_word = first_word(s);
+
+    println!("{}", first_word);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
